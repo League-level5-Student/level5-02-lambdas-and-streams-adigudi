c96ae25e5610a5c491978e6775cb2286d6258454
League Student <league@iMac-27.attlocal.net>
2020-01-12 10:14:45 -0800

    FInished module

--- src/_04_Searching_With_Streams/StreamSearcher.java
+++ src/_04_Searching_With_Streams/StreamSearcher.java
@@ -3,6 +3,7 @@ package _04_Searching_With_Streams;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.Stream;
+import java.util.function.Predicate;
 
 public class StreamSearcher {
 	// 1. This method takes an array of Strings and a String. The method returns the number
@@ -10,12 +11,18 @@ public class StreamSearcher {
 	// use Streams to help us out.
 	public long containsCount(String[] strArr, String strToCount) {
 		//2. Convert the String array to a stream
-		
+		Stream<String> strs = Arrays.stream(strArr);
 		//3. Use the filter method with a lambda to extract all the String
 		//   elements that match the String parameter.
-		
+		return strs.filter((b) -> {
+			if(b == strToCount) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}).count();
 		//4. Return the count of the filtered stream.
 		
-		return 0;
 	}
 }
